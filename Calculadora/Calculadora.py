153b96f977ea68805e170c0e870904e7b599270e
JOEL FRANCO MUNOZ <al02901595@tecmilenio.mx>
2021-10-27 13:28:21 -0500

    .

--- Calculadora/Calculadora.py
+++ Calculadora/Calculadora.py
@@ -18,8 +18,18 @@ print("Intruduce el primer numero")
 n1 =int (input())
 print("Intruduce el segundo numero")
 n2 =int (input())
+print(type (n1))
 print(Selec_Operacion)
 Op = input()
 
-#if Op == "Suma":
-    
+if Op == "Suma":
+    print("la suma de {} + {} tiene como resultado {} ".format(n1,n2,n1+n2))
+if Op == "Resta":
+    print("la resta de {} - {} tiene como resultado {} ".format(n1,n2,n1-n2))
+if Op == "Multiplicacion":
+    print("la multiplicacion de {} * {} tiene como resultado {} ".format(n1,n2,n1*n2))
+if Op == "Division":
+    if n1 == 0 or n2 == 0:
+        print("Dividir entre cero o dividir cero no esta permitido intente nuevamente")
+    else:
+        print("las division de {} / {} tiene como resultado {} ".format(n1,n2,n1/n2))
