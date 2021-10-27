340c6c597317d13a04fb06c32cb553a68b202fde
bywer <bywer@OMEN-FRANCO>
2021-10-27 12:58:09 -0500

    Agregar archivos de proyecto.

---
+++ Calculadora/Calculadora.py
@@ -0,0 +1,30 @@
+
+
+while True: 
+    Intro = """
+          CALCULADORA
+
+Para utilizar la calculadora es necesario introducir dos numeros
+a los cuales vamos a operar, despues seleccionar la operacion que 
+se quiere realizar
+"""
+    Selec_Operacion ="""
+Selecciona la operacion de su eleccion
+1.- Suma
+2.- Resta
+3.- Multiplicacion
+4.- Division
+"""
+
+    print(Intro)
+    print("Intruduce el primer numero")
+    n1 = input
+    print("Intruduce el segundo numero")
+    n2 = input
+    print(Selec_Operacion)
+    Op = input
+    #Op = x
+
+    #while x == 0: #Suma 
+
+
