e1bbace08d32839f60575b42aa0ea54bf48320e3
JOEL FRANCO MUNOZ <al02901595@tecmilenio.mx>
2021-10-27 13:15:12 -0500

    segunda prueba calculadora

--- Calculadora/Calculadora.py
+++ Calculadora/Calculadora.py
@@ -1,30 +1,25 @@
 
-
-while True: 
-    Intro = """
+Intro = """
           CALCULADORA
 
 Para utilizar la calculadora es necesario introducir dos numeros
 a los cuales vamos a operar, despues seleccionar la operacion que 
 se quiere realizar
 """
-    Selec_Operacion ="""
+Selec_Operacion ="""
 Selecciona la operacion de su eleccion
 1.- Suma
 2.- Resta
 3.- Multiplicacion
 4.- Division
 """
+print(Intro)
+print("Intruduce el primer numero")
+n1 =int (input())
+print("Intruduce el segundo numero")
+n2 =int (input())
+print(Selec_Operacion)
+Op = input()
 
-    print(Intro)
-    print("Intruduce el primer numero")
-    n1 = input
-    print("Intruduce el segundo numero")
-    n2 = input
-    print(Selec_Operacion)
-    Op = input
-    #Op = x
-
-    #while x == 0: #Suma 
-
-
+#if Op == "Suma":
+    
